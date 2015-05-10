39ce26ae84b78d5c0d96c8c8dea4dd9d7aef5baf
Andrei Darien Elliott <a.d.elliott@ku.edu>
2015-05-09 22:17:54 -0500

    added context to comment

--- enemy.py
+++ enemy.py
@@ -1,7 +1,7 @@
 import ship
 class enemy_object (ship.ship_object):
     def __init__(self,art,context_size, context,ai,weapon,proj,mvmt_ptrn):
-        self.weapon       = weapon([0,0],proj,mvmt_ptrn,context,context_size) # TODO: add proper initialization parameters
+        self.weapon       = weapon([0,0],proj,mvmt_ptrn,context,context_size) # TODO: add proper initialization parameters  -- should go in calling context (enemy_holder) at least for ai
         ship.ship_object.__init__(self,art,context_size, context, self.weapon)
         self.ai = ai
         self.pos = self.ai.getStartPos(context_size)
