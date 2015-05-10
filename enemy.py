032bd229f3d2f9528ab68e8fbdce1323f7353d91
Andrei Darien Elliott <a.d.elliott@ku.edu>
2015-05-09 19:38:23 -0500

    fix to enemy_object class

--- enemy.py
+++ enemy.py
@@ -1,11 +1,11 @@
 import ship
 class enemy_object (ship.ship_object):
-    def __init__(self,art,context_size,ai,weapon):
-        ship.ship_object.__init__(self,art,context_size,weapon)
+    def __init__(self,art,context_size, context, ai,weapon):
+        ship.ship_object.__init__(self,art,context_size, context, weapon)
         self.ai = ai
-        self.x, self.y = self.ai.getStartPos(context_size)
+        self.pos = self.ai.getStartPos(context_size)
 
-    def update():
-        self.x, self.y = self.ai.getNextPos(self.x, self.y)
+    def update(self):
+        self.pos = self.ai.getNextPos(self.pos)
         
                 
