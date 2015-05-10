adbf6f51d66e4af093ec562fb1eee8f286c2b786
nshaheed <n704s313@ku.edu>
2015-05-10 12:53:19 -0500

    v0.95 added sin_ai

--- ai.py
+++ ai.py
@@ -38,22 +38,27 @@ class circling_ai(ai):
         
 class sin_ai(ai):
     # moves left to right in a sine pattern.
-    def __init__(self, amplitude = 50, speed = 0.1):
-        self.amplitude = amplitude
+    def __init__(self, speed = 0.1):
+        self.amplitude = random.randint(25,context_size[0] - 50)
         self.speed     = speed
         self.angle     = 0
         
     # make sure ai doesn't spawn along the x axis closer than the amplitude of the ai
     def getStartPos(self, context_size):
-        self.center = [random.randint(amplitude, context_size[0] - amplitude), random.randint(0, context_size[1]/2)]
+        print(str(context_size[0]) + "," + str(self.amplitude))
+        randx = random.randint(self.amplitude, context_size[0] - self.amplitude)
+        print(randx)
+        self.center = [randx, random.randint(0, context_size[1]/2)]
         return self.center
         
     def getNextPos(self, pos):
-        self.angle += self.angle
-        return [math.sin(self.angle), self.center[1]]
+        # print("sin_ai called")
+        self.angle += self.speed
+        # print("angle:    " + str(self.angle))
+        return [math.sin(self.angle) * self.amplitude + self.center[0], self.center[1]]
         
 
 
 
 # a list of ai constructors along with parameters - when generating a new enemy, choose randomly from among these 
-aiList = [(ai,[]),(circling_ai,[]),(circling_ai,[50,-.1]),(sin_ai,[])]
+aiList = [(ai,[]),(circling_ai,[]),(circling_ai,[50,-.1]),(sin_ai,[0.1])]
